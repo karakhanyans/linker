9080e420e2c2bf70793d20a5022a18e1ea3e94b9
Sergey Karakhanyan <karakhanyansa@gmail.com>
2016-04-13 13:44:54 +0400

    new email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,5 +1,131 @@
-Hello {{ $name }} {{ $lastname }}
-Welcome to the Linker.
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="../../../www.w3.org/1999/xhtml/index.html">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" name="viewport">
+    <title>Welcome to Linker</title>
+    <link rel="stylesheet" type="text/css" href="https://gallery.mailchimp.com/4fa1ba9f034b320a2f7e3b780/files/style.css">
+</head>
+<body yahoofix="" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- START OF PAGE WRAPPER -->
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#343739">
+    <tr>
+        <td valign="top">
+            <!-- START OF EMAIL WRAPPER -->
+            <table width="650" border="0" align="center" cellpadding="0" cellspacing="0" class="emailWrapper" bgcolor="#F9F9F9">
+                <tr>
+                    <td bgcolor="#F9F9F9">
 
-Your email - {{ $email }}
-Your password - {{ $password }}
+                        <!-- START OF CONTENT WRAPPER -->
+                        <table width="630" border="0" align="center" cellpadding="0" cellspacing="0" class="contentWrapper" bgcolor="#F9F9F9">
+                            <tr>
+                                <td bgcolor="#F9F9F9">
+                                    <!-- START OF VIEW ONLINE WRAPPER -->
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="20" align="center" bgcolor="#42484D">
+                                        <tr>
+                                            <td valign="top" align="center" bgcolor="#42484D">
+                                                <!-- START OF VIEW ONLINE -->
+                                                <table width="630" align="center" border="0" cellspacing="0" cellpadding="0" bgcolor="#42484D" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;">
+                                                    <tr>
+                                                        <td valign="top" align="center" bgcolor="#42484D"><a href="#" style="color: #ffffff;"><img src="https://gallery.mailchimp.com/4fa1ba9f034b320a2f7e3b780/images/3dd2df66-bf30-4b9b-baa1-d36577794ed2.png" alt="your logo text here" width="120"></a></td>
+                                                    </tr>
+                                                </table>
+                                                <!-- END OF VIEW ONLINE -->
+                                            </td></tr>
+                                        <tr>
+                                        </tr></table>
+                                    <!-- END OF VIEW ONLINE WRAPPER -->
+
+
+                                    <!-- START OF SPLASH IMAGE -->
+                                    <table width="630" border="0" cellspacing="0" cellpadding="0" align="center" bgcolor="#F9F9F9" class="mobileCenter">
+                                        <tr>
+                                            <td valign="top" align="center" bgcolor="#26ae90">
+                                                <h1 style="font-family: Helvetica, Arial, sans-serif; color: #fff; font-size: 35px; font-weight: 200; vertical-align:top;">Welcome to Linker</h1>
+                                            </td>
+
+                                        </tr>
+                                    </table>
+                                    <!-- END OF SPLASH IMAGE -->
+
+
+
+
+                                    <!-- START SPACER -->
+                                    <table width="630" border="0" cellpadding="0" cellspacing="0" class="hr" align="center" bgcolor="#F9F9F9">
+                                        <tr>
+                                            <td height="30" align="left" valign="middle"></td>
+                                        </tr>
+                                    </table>
+                                    <!-- END SPACER -->
+
+                                    <!-- START OF FULL WIDTH -->
+                                    <table width="630" border="0" cellspacing="0" cellpadding="0" align="center">
+                                        <tr>
+                                            <td height="15" bgcolor="#F9F9F9"></td>
+                                        </tr>
+                                        <tr>
+                                            <td valign="top" align="left" width="630" style="font-family: Helvetica, Arial, sans-serif; color: #26ae90; font-size: 22px; font-weight: 200; text-align:left; vertical-align:top;" class="heading1">Hi {{ $name }} {{ $lastname }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td height="10" bgcolor="#F9F9F9"></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="630" valign="top" align="left" style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; color: #808080; line-height: 24px; font-weight: 200; text-align:left; vertical-align:top;" class="textIndent">Your email - <a href="#" style="color:#26ae90;">{{ $email }}</a></td>
+
+                                        </tr>
+                                        <tr>
+                                            <td width="630" valign="top" align="left" style="font-family: Helvetica, Arial, sans-serif; font-size: 14px; color: #808080; line-height: 24px; font-weight: 200; text-align:left; vertical-align:top;" class="textIndent">Your password - <a href="#" style="color:#26ae90;">{{ $password }}</a></td>
+                                        </tr>
+                                        <tr>
+                                            <td height="15" bgcolor="#F9F9F9"></td>
+                                        </tr>
+                                    </table>
+                                    <!-- END OF FULL WIDTH -->
+
+
+
+
+                                    <!-- START SPACER -->
+                                    <table width="630" border="0" cellpadding="0" cellspacing="0" class="hr" align="center" bgcolor="#F9F9F9">
+                                        <tr>
+                                            <td height="40" align="left" valign="middle"></td>
+                                        </tr>
+                                    </table>
+                                    <!-- END SPACER -->
+
+
+
+
+
+
+                                    <!-- START OF COPYRIGHT WRAPPER -->
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" bgcolor="#42484D">
+                                        <tr>
+                                            <td valign="top" align="center" bgcolor="#42484D">
+
+                                                <!-- START OF COPYRIGHT -->
+                                                <table width="630" align="center" border="0" cellspacing="0" cellpadding="0" bgcolor="#42484D" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;">
+                                                    <tr>
+                                                        <td valign="top" align="center" bgcolor="#42484D" style="font-family: Helvetica, Arial, sans-serif; font-size: 11px; color: #868A8D; padding: 20px 0 20px 0; line-height: 20px; font-weight: 200; text-align:center; vertical-align:top;">
+                                                            &copy; <a href="http://img.mailchimp.com/404-keepyourlink.com" style="color: #ffffff;">Linker Corporation</a> 2016 - Created by Sergey Karakhanyan</td>
+                                                    </tr>
+                                                </table>
+                                                <!-- END OF COPYRIGHT -->
+                                            </td></tr>
+                                        <tr>
+                                        </tr></table>
+                                    <!-- END OF COPYRIGHT WRAPPER -->
+
+
+                                </td>
+                            </tr>
+                        </table><!-- END OF EMAIL WRAPPER -->
+                    </td>
+                </tr>
+            </table><!-- END OF CONTENT WRAPPER -->
+        </td>
+    </tr>
+</table><!-- END OF PAGE WRAPPER -->
+</body>
+</html>
